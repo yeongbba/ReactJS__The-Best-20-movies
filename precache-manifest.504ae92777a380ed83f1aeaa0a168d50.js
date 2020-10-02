09fafc3dde7fc4e6c80e23789caeaa16722353b8
yeongbba <yeongmolee2@gmail.com>
2020-10-02 17:03:26 +0900

    Updates

---
+++ precache-manifest.504ae92777a380ed83f1aeaa0a168d50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d2feda89a02236207dba8376a4f3cf2",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "006bed29c72b69f26c74",
+    "url": "/movie-app/static/css/main.888d1aad.chunk.css"
+  },
+  {
+    "revision": "1c1867ee385e1fd15da6",
+    "url": "/movie-app/static/js/2.3a05f077.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.3a05f077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006bed29c72b69f26c74",
+    "url": "/movie-app/static/js/main.7445d3b5.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  }
+]);
